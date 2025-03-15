f2ad4fbfad03c6bb5389ac73354d1cfcdf5229e8
arjbarber <arjbarber@gmail.com>
2025-03-15 11:02:23 -0400

    formatting changes

--- main.py
+++ main.py
@@ -2,17 +2,19 @@ import pygame
 import cv2
 import config
 import sys
-
+import time
+import os
 pygame.init()
 
-WIN = pygame.display.set_mode((config.WIDTH, config.HEIGHT))#,pygame.FULLSCREEN)
+WIN = pygame.display.set_mode((config.WIDTH, config.HEIGHT))
 
-def draw_screen(frame):
+def draw_screen(frame, capture_button: pygame.Rect):
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     frame = cv2.transpose(frame)
     frame = pygame.surfarray.make_surface(frame)
     frame = pygame.transform.scale(frame, (config.WIDTH, config.HEIGHT))
     WIN.blit(frame, (0, 0))
+    pygame.draw.circle(WIN, config.CAPTURE_BUTTON_COLOR, capture_button.center, config.CAPTURE_BUTTON_SIZE // 2)
     pygame.display.update()
 
 def initialize_camera():
@@ -26,9 +28,14 @@ def initialize_camera():
 
 def main():
     pygame.display.set_caption("Pygame Camera App")
-    #capture_button = pygame.rect.Rect(config.)
-    clock = pygame.time.Clock()
+    capture_button = pygame.rect.Rect(
+        config.WIDTH // 2 - config.CAPTURE_BUTTON_SIZE // 2,
+        config.HEIGHT - config.CAPTURE_BUTTON_SIZE - config.CAPTURE_BUTTON_PADDING,
+        config.CAPTURE_BUTTON_SIZE,
+        config.CAPTURE_BUTTON_SIZE,
+    )
 
+    clock = pygame.time.Clock()
     cap = initialize_camera()
 
     run = True
@@ -42,10 +49,12 @@ def main():
         if not ret:
             print("Error: Could not read frame.")
             break
-        
 
-        draw_screen(frame)
-    
+        if wait_until_blink(frame):
+            print("Blink detected!")
+
+        draw_screen(frame, capture_button)
+
     cap.release()
     pygame.quit()
 
